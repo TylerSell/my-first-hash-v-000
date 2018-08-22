4cd53399a362811fc95396ee2003d974f164733d
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-08-22 18:01:51 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,6 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  hash_1 = {"first name" => "Tyler", "last name" => "Sell"}
+  hash_1
 end
 
 
